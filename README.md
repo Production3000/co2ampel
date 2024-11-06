071e21b6d28866191ebb1431a3d3d1f662e1d4a5
ferdinand0815 <73479504+ferdinand0815@users.noreply.github.com>
2024-11-06 17:01:31 +0100

    Readme, documentation in code

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# co2ampel
+# MVP3000 Application Note: CO2-Lights (CO2-Ampel)
+
+The CO2-Lights serves as a showcase application that highlights the versatility of the [MVP3000](https://github.com/Production3000/mvp3000esp) framework. This implementation uses the sensor module and the LED module. It features a custom landing page accessible via any smartphone or browser. The page provides real-time ambient CO2 concentration data alongside a historical graph, which updates automatically through WebSocket.
+
+See also [here](https://co2ampel.production3000.com/) for additional information and images.
+
+
+## Hardware
+
+The original CO2-Ampel was developed by purecrea GmbH and is available via [Bastelgarage.ch](https://www.bastelgarage.ch/co2-ampel-mit-smileys-und-balkenanzeige) and [co2ampel.ch](https://www.co2ampel.ch). The [official firmware](https://github.com/bastelgarage/co2ampel) is open source as well.
+
+Specifications:
+ *  ESP8266 micro controller
+ *  [Sensirion SDC30](https://sensirion.com/products/catalog/SCD30) CO2 sensor, additionally measuring temperature and relative humidity. See also the reference [implementation](https://github.com/Sensirion/arduino-i2c-scd30).
+ *  WS2812B strip with 12 LED
+ *  Photo resistor as ambient light sensor
+ *  Push button
+
+
+## Implementation
+
+The implementation consists of two main components: the main script and a web display for the end-user. The [MVP3000](https://github.com/Production3000/mvp3000esp) framework manages essential tasks such as data processing, connectivity, and communication.
+
+### Main Script
+
+The main script on the ESP8266 delivers core functionality. Please refer to the comments in the [code](/co2ampel.ino) for detailed explanations.
+
+ *  Initialize the MVP3000 framework and its modules.
+ *  Interface with the CO2 sensor, conduct measurements, and pass the collected data to the sensor module of the framework.
+ *  Provide a callback to the sensor module of the framework to handle sensor calibration upon user request (via landing page). 
+ *  Define CO2 concentration thresholds and set the LED colors using the LED module of the framework.
+
+
+### Web Display
+
+The web display for end-users, hosted on the ESP8266, provides an intuitive landing page that displays current CO2 concentration and a recent data graph, with live updates via WebSocket. 
+
+The HTML, CSS, JavaScript, and image files are stored in the [/landing_page](/landing_page/) folder for quick design development. The JavaScript classes *MicroPlot3000* and *WebSocket3000* are tools provided by the [MVP3000](https://github.com/Production3000/mvp3000esp) framework. The Python script *webpagebuilder.py*, also part of the framework, can be used to combine all files into a single string and automatically insert it into the ESP8266 code.
+
+    python3 webpagebuilder.py
+
+To access the landing page browse to the IP address of the ESP8266. Make sure to be connected to the same network as the ESP8266, either its own access point or if credentials are given the shared network.
+
+    http://[deviceIp] 
+
+All MVP3000 settings are moved to a sub-URL.
+
+    http://[deviceIp]/mvp3000
+
+
+## License
+
+Licensed under the Apache License. See `LICENSE` for more information.
+
+Copyright Production 3000
--- co2ampel.ino
+++ co2ampel.ino
@@ -14,20 +14,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+
 /// Sensor specific code /////////////////////////////////////////////////////////////
 
-// Sensirion I2C SCD30 https://github.com/Sensirion/arduino-i2c-scd30
-#include <SensirionI2cScd30.h>
 #include <Wire.h>
+#include <SensirionI2cScd30.h> // Sensirion I2C SCD30 https://github.com/Sensirion/arduino-i2c-scd30
 SensirionI2cScd30 sdc30;
 
-uint16_t atmosphericCO2 = 419; // ppm
+uint16_t atmosphericCO2 = 419; // environmental CO2 ppm for calibration
 uint16_t measurementInterval = 2; // s
 uint16_t siteAltitude = 450; // m
 uint16_t temperatureCompensation = 350; // °C*100
 
 // Sensor data is: CO2, T, rH
 const uint8_t valueCount = 3;
+
+// Local data variable
+float_t data[valueCount];
 
 
 /// MVP3000 Framwork and Modules /////////////////////////////////////////////////
@@ -46,23 +49,20 @@ uint8_t calibrationCounter = 0;
 /// Sensor module ////////////////////////////////////////////////////////////////
 #include <XmoduleSensor/XmoduleSensor.h>
 
+// Initialize the sensor module with the number of values
+XmoduleSensor xmoduleSensor(valueCount);
+
 // Add a description of the sensor for the web interface
 String infoName = "Sensirion SDC30";
 String infoDescription = "Sensirion SDC30 ambient CO2 sensor module, measures also temperature and relative humidity.";
 String sensorTypes[valueCount] = {"CO2", "T", "rH"};
 String sensorUnits[valueCount] = {"ppm", "0.1 &deg;C", "0.1 &percnt;"};
 
-// Local data variable
-float_t data[valueCount];
-
-// Convert native sensor units to the desired units
+// Conversion of native sensor units to the desired integer units
 //  ppm
 //  °C -> 0.1 °C
 //  % -> 0.1 %
 int8_t exponent[valueCount] = {0, 1, 1};
-
-// Init sensor module
-XmoduleSensor xmoduleSensor(valueCount);
 
 
 /// LED module ////////////////////////////////////////////////////////////////
@@ -71,8 +71,10 @@ XmoduleSensor xmoduleSensor(valueCount);
 const uint8_t ledPin = D8; // NeoPixels pin
 const uint8_t ledCount = 12; // Number of NeoPixels
 
+// Initialize the LED module with the number of LED on the strip
 XmoduleLED xmoduleLED(ledPin, ledCount);
 
+// Standard colors for the LED smileys and bars
 uint32_t standardColors[ledCount] = {
     // LED smileys: red, yellow, green
     ColorRGB(255, 0, 0),
@@ -90,38 +92,51 @@ uint32_t standardColors[ledCount] = {
     ColorRGB(255, 0, 0)
 };
 
+// Flag for very high CO2 concentrations
+bool isBlinking = false;
+
 
 void setup() {
+
+    /// MVP3000 Framwork and Modules /////////////////////////////////////////////////
+
     // Init the sensor module and add it to the mvp framework
     xmoduleSensor.setSensorInfo(infoName, infoDescription, sensorTypes, sensorUnits);
     xmoduleSensor.setSampleAveraging(1); // The SCD30 has already averaged data ready every 1.5 s. Averaging in the framework can be set to a low value.
     xmoduleSensor.setReportingThreshold(5, 0); // Recording threshold change of 0.5 percent of the CO2 value
     xmoduleSensor.setSampleToIntExponent(exponent);
-    xmoduleSensor.setNetworkCtrlCallback(networkCtrlCallback);
+    xmoduleSensor.setNetworkCtrlCallback(networkCtrlCallback); // Add a callback for network control commands / calibration 
     mvp.addXmodule(&xmoduleSensor);
 
-    // Add the custom module to the mvp framework
-    mvp.addXmodule(&xmoduleLED);
-    xmoduleLED.adaptiveGlobalBrightness(A0);
+    // Init the led module and add it to the mvp framework
+    xmoduleLED.adaptiveGlobalBrightness(A0); // Use a potentiometer on A0 to adjust the global brightness
     xmoduleLED.setFixedColorIndividual(standardColors);
+    mvp.addXmodule(&xmoduleLED);
 
-    // CO2-Ampel landing page
+    // Set the CO2-Ampel landing page, mvp settings are automatically moved
     mvp.setAlternateRoot(altResponseFiller);
+
+    // Disable MQTT and UDP discovery
     mvp.mqttHardDisable();
     mvp.udpHardDisable();
 
     // Start mvp framework
     mvp.setup();
 
+
+    /// Sensor specific code /////////////////////////////////////////////////////////////
+
     // I2C
     Wire.begin();
 
     // Init SCD30 sensor
     sdc30.begin(Wire, SCD30_I2C_ADDR_61);
-    // sdc30.stopPeriodicMeasurement();
+    
+    // Sometimes the sensor does not come up, maybe only after flashing.
     int16_t sdcError = sdc30.softReset(); // Blocking 2000 ms
     if (sdcError != NO_ERROR) {
         operatingState = OperatingState::ERROR;
+        // Indicate error by red LED
         xmoduleLED.setFixedColorSync(ColorRGB(255, 0, 0));
         static char errorMessage[128];
         errorToString(sdcError, errorMessage, sizeof errorMessage);
@@ -129,12 +144,13 @@ void setup() {
         return;
     }
 
+    // Set sensor settings. Those are saved by the SDC30 and possibly different depending on previous uploaded firmware.
     operatingState = OperatingState::MEASURE;
     sdc30.setMeasurementInterval(measurementInterval);
     sdc30.setAltitudeCompensation(siteAltitude);
     sdc30.setTemperatureOffset(temperatureCompensation);
 
-    // Get and print sensor information
+    // Get and print SDC30 sensor information
     uint8_t major = 0;
     uint8_t minor = 0;
     sdc30.readFirmwareVersion(major, minor);
@@ -146,7 +162,6 @@ void setup() {
     sdc30.getTemperatureOffset(temperatureOffset);
     // uint16_t co2RefConcentration;
     // sdc30.getForceRecalibrationStatus(co2RefConcentration); // This always returns 400 after softReset/power cycle. However, the sensor still uses the updated calibration curve.
-
     uint16_t isActive;
     sdc30.getAutoCalibrationStatus(isActive);
     mvp.logFormatted("SDC30: Firmware version %d.%d, interval %d s, altitude %d m, temp offset %d C, auto calibration %s", major, minor, interval, altitude, temperatureOffset, (isActive) ? "active" : "disabled");
@@ -158,7 +173,7 @@ void setup() {
     sdc30.setAltitudeCompensation(siteAltitude);
 }
 
-bool isBlinking = false;
+
 void loop() {
     // Do the work
     mvp.loop();
@@ -167,21 +182,31 @@ void loop() {
         return;
     }
 
+    // Check if there is data available 
     uint16_t sdcDataReady;
     sdc30.getDataReady(sdcDataReady);
     if (sdcDataReady) {
+
+        // Check if the data can be read
         if (sdc30.readMeasurementData(data[0], data[1], data[2]) == NO_ERROR) {
+
+            // Action depending on the operating state
             if (operatingState == OperatingState::MEASURE) {
-                // Add data to the sensor module
+                // Check if data makes sense
                 // The first CO2 measurement is always 0, probably some running median thing
                 if (data[0] > atmosphericCO2 - 100) {
+                    // Add data to the sensor module
                     xmoduleSensor.addSample(data);
+
+                    // Set LED brightness: the color is fixed, LED are just switched on/off using brightness
                     if (data[0] < 1500) {
+                        // For small concentrations set the LED brightness according to the CO2 value
                         isBlinking = false;
                         uint8_t brightness[ledCount] = {0};
-                        calcLedBrightness(brightness);
+                        selectLedBrightness(brightness);
                         xmoduleLED.setFixedBrightnessIndividual(brightness);
                     } else {
+                        // For very high concentrations start the blinking-brightness-effect (once)
                         if (!isBlinking) {
                             isBlinking = true;
                             xmoduleLED.setBrightnessEffect(1000, XledFx::BRIGHTNESSFX::PULSE_FULL);
@@ -189,14 +214,18 @@ void loop() {
                     }
                 }
             }
+
+            // Action depending on the operating state
             if (operatingState == OperatingState::CALIBRATE) {
-                // The sensor needs 1-3 measurement cycles to update the calibration curve
+                // The sensor needs 1-2 measurement cycles to update the calibration curve
                 if (calibrationCounter++ > 2) {
                     operatingState = OperatingState::MEASURE;
+                    // Restore default LED behavior
                     xmoduleLED.setFixedBrightnessSync(255);
                     xmoduleLED.setFixedColorIndividual(standardColors);
                 }
             }
+
         } else {
             mvp.log("Error trying to execute readMeasurementData()");
         }
@@ -204,6 +233,7 @@ void loop() {
 }
 
 
+/// Webpage ////////////////////////////////////////////////////////////////
 #include "webpage.h"
 size_t altResponseFiller(uint8_t *buffer, size_t maxLen, size_t index) {
     // Chunked response filler for the html template
@@ -215,6 +245,7 @@ size_t altResponseFiller(uint8_t *buffer, size_t maxLen, size_t index) {
     return maxLen;
 }
 
+/// WebSocket callback /////////////////////////////////////////////////////////////
 void networkCtrlCallback(const String& data) {
     if (data.equals("CALIBRATE")) {
         calibrateSensor();
@@ -223,26 +254,29 @@ void networkCtrlCallback(const String& data) {
     }
 }
 
-
+/// Calibration ////////////////////////////////////////////////////////////////
 void calibrateSensor() {
-    // Data sheet states 2 minutes for the sensor. Could be more with housing.
+    // Data sheet states to operate the sensor at least 2 minutes on fresh air. Could be more with housing.
     if (millis() < 3*60*1000) {
         mvp.log("Calibration not started. The device needs to be OPERATING at FRESH air for AT LEAST 3 minutes before calibration!");
         return;
     }
 
-    // Set calibrating step
-
+    // Start calibration
     int16_t sdcError = sdc30.forceRecalibration(atmosphericCO2); // Blocking 10 ms
     if (sdcError != NO_ERROR) {
+        // Fail
         operatingState = OperatingState::ERROR;
+        // Indicate error by red LED
         xmoduleLED.setFixedColorSync(ColorRGB(255, 0, 0));
         static char errorMessage[128];
         errorToString(sdcError, errorMessage, sizeof errorMessage);
         mvp.logFormatted("Error during calibration: %s", errorMessage);
     } else {
+        // Calibration started, the sensor needs 1-2 measurement cycles to update the calibration curve
         operatingState = OperatingState::CALIBRATE;
-    calibrationCounter = 0;
+        calibrationCounter = 0;
+        // Indicate calibration by pulsing the LED blue
         xmoduleLED.setFixedColorSync(ColorRGB(0, 0, 255));
         xmoduleLED.setBrightnessEffect(1000, XledFx::BRIGHTNESSFX::PULSE_FULL);
         mvp.logFormatted("Calibration to %d ppm initiated. Restarting measurement ...", atmosphericCO2);
@@ -250,8 +284,9 @@ void calibrateSensor() {
 }
 
 
-void calcLedBrightness(uint8_t* brightness) {
-    // LED bars
+/// Select LED brightness depending on CO2 concentration /////////////////////////////////////////////////////////////
+void selectLedBrightness(uint8_t* brightness) {
+    // LED bars, light all below value
     switch ((uint16_t)data[0]) {
         case 1150 ... 65535: // ggyyrr
             brightness[11] = 255;
@@ -269,7 +304,7 @@ void calcLedBrightness(uint8_t* brightness) {
             break;
     }
 
-    // LED smileys
+    // LED smileys, light selectively
     switch ((uint16_t)data[0]) {
         case 1500 ... 65535: // r + blink
         case 1150 ... 1499: // r
